50af69e0eba8a00a5ea99cd42403d1a93ce3bbdb
Ma77h3hac83r <ma77h3hac83r@gmail.com>
2025-11-23 16:03:32 -0800

    revert from astro 5 to 4, update dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,16 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: cloudflare(),
+  adapter: cloudflare({
+    platformProxy: {
+      enabled: true,
+    },
+  }),
   integrations: [tailwind()],
+  vite: {
+    ssr: {
+      external: ['bcryptjs'],
+    },
+  },
 });
 
